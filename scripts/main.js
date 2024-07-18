96ec56811d0bccdd04df13a47c3018e0982a73fe
phendges7 <uk_hendges@outlook.com>
2024-07-18 09:54:48 +0100

    adjust popup
    Fixed main.js to open and close popup
    Adjusted popup.css to make it floating on top of layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,19 @@
+/*Popup Variaveis*/
 let page = document.querySelector(".page");
-let editButton = page.querySelector(".profile__edit-profile-button");
+let editProfileButton = page.querySelector(".profile__edit-profile-button");
+let popup = page.querySelector(".popup");
+let closeButton = popup.querySelector(".popup__close-button");
 
-function openPopup() {
-  page.insertAdjacentHTML(
-    "afterbegin",
-    `<form class="profile popup">
-            <img
-              src="./images/closeIcon.svg"
-              alt="Fechar"
-              class="popup__close-button"
-            />
-            <div class="popup__wrapper">
-              <h3 class="popup__title">EDITAR PERFIL</h3>
-              <input
-                type="text"
-                class="popup__input popup__user-name"
-                placeholder="Nome"
-              />
-              <input
-                type="text"
-                class="popup__input popup__user-description"
-                placeholder="Sobre mim"
-              />
-              <button type="submit" class="popup__submit-button">
-                SALVAR
-              </button>
-            </div>
-          </form>`
-  );
-  alert("CLICOU");
+/*Popup Funcoes*/
+export function closePopup() {
+  popup.classList.remove("visible");
+  console.log("FECHOU");
+}
+export function openPopup() {
+  popup.classList.add("visible");
+  console.log("CLICOU");
 }
 
-editButton.addEventListener("click", openPopup);
+/*Ativando funcoes*/
+editProfileButton.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closePopup);
