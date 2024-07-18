5e5709763ac692993ea28ad4a10d3f635a12bc25
phendges7 <uk_hendges@outlook.com>
2024-07-18 10:48:01 +0100

    Adjust OVERLAY

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,23 @@
 /*Popup Variaveis*/
 let page = document.querySelector(".page");
+let overlay = document.querySelector(".overlay");
 let editProfileButton = page.querySelector(".profile__edit-profile-button");
 let popup = page.querySelector(".popup");
 let closeButton = popup.querySelector(".popup__close-button");
 
-/*Popup Funcoes*/
-export function closePopup() {
-  popup.classList.remove("visible");
-  console.log("FECHOU");
-}
-export function openPopup() {
+/* Abrir/Fechar */
+function openPopup() {
+  overlay.classList.add("visible");
   popup.classList.add("visible");
   console.log("CLICOU");
 }
 
-/*Ativando funcoes*/
+function closePopup() {
+  popup.classList.remove("visible");
+  overlay.classList.remove("visible");
+  console.log("FECHOU");
+}
+
+/* Ativando funcoes */
 editProfileButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
